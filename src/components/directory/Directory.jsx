e287cc29a6b77903637783c1d33f655b0db5bed9
MrAnAcondA <shahzebjadoon0@gmail.com>
2022-04-20 12:38:55 +0500

    added directory component and split code

---
+++ src/components/directory/Directory.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './directory.scss';
+import CategoryItem from '../category-item/CategoryItem';
+
+const Directory = ({ categories }) => {
+  return (
+    <div className='directory-container'>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
